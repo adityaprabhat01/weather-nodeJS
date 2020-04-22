e55e1fc993e0042ed9a83ccd697a3efe077d8715
Aditya Prabhat <adityaprabhat1998@gmail.com>
2020-04-22 13:04:34 +0530

    update heroku

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const geocode = require('./utils/geocode')
 const weather = require('./utils/weather')
 const app = express()
+const port = process.env.PORT || 3000
 const hbs = require('hbs')
 
 
@@ -82,6 +83,6 @@ app.get('*', (req, res) => {
 
 
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
